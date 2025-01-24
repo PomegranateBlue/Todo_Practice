e4f214b2b182168eedb4769554b90cc7d833f665
bluepome98 <wpdn1998@gmail.com>
2025-01-24 19:26:32 +0900

    로컬 스토리지 저장 시 삭제 후의 상태 저장 안 되고 있음

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ function App() {
   //원본이 되는 데이터로서 해당 데이터의 복사본을 이용할 것
   const [newTodo, setNewTodo] = useState("");
   //할 일은 newTodo를 통해 상태를 확인하고 id,schedule 필드로 이루어진 데이터를 추가한다.
-
+  window.onload = () => {
+    const loadTodos = localStorage.getItem("savedTodos");
+    if (loadTodos) {
+      setTodo(JSON.parse(loadTodos));
+    }
+  };
   const addTodo = (e) => {
     e.preventDefault();
     const newSchedule = {
@@ -15,13 +20,12 @@ function App() {
       schedule: newTodo,
     };
     //newSchedule이라는 객체는 새로 추가되는 항목의 상태인 newTodo 상태를 받는다
-    setTodo([...todo, newSchedule]);
+    const updateTodo = [...todo, newSchedule];
+    setTodo(updateTodo);
+    localStorage.setItem("savedTodos", JSON.stringify(updateTodo));
     setNewTodo("");
   };
   const deleteTodo = (id) => {
-    //id값의 확인을 통해 삭제한다.
-    // const deleteTodo = todo.filter((item) => item.id !== id);
-    // setTodo([deleteTodo]);
     setTodo(todo.filter((item) => item.id !== id));
   };
   const handleInputValue = (e) => {
