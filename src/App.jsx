2657f21c383bcd6dcf683ad190a7a1b415037fee
bluepome98 <wpdn1998@gmail.com>
2025-01-24 18:56:48 +0900

    삭제하면 전부 사라짐

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   //할 일 목록 저장
   //원본이 되는 데이터로서 해당 데이터의 복사본을 이용할 것
   const [newTodo, setNewTodo] = useState("");
+  //할 일은 newTodo를 통해 상태를 확인하고 id,schedule 필드로 이루어진 데이터를 추가한다.
 
   const addTodo = (e) => {
     e.preventDefault();
@@ -13,10 +14,15 @@ function App() {
       id: Date.now(),
       schedule: newTodo,
     };
+    //newSchedule이라는 객체는 새로 추가되는 항목의 상태인 newTodo 상태를 받는다
     setTodo([...todo, newSchedule]);
     setNewTodo("");
   };
-
+  const deleteTodo = (id) => {
+    //id값의 확인을 통해 삭제한다.
+    const deleteTodo = todo.filter((item) => item.id !== id);
+    setTodo([deleteTodo]);
+  };
   const handleInputValue = (e) => {
     setNewTodo(e.target.value);
   };
@@ -39,7 +45,7 @@ function App() {
         {todo.map((item) => (
           <li key={item.id}>
             {item.schedule}
-            <button type="button" onClick={(e) => console.log(e.target)}>
+            <button type="button" onClick={deleteTodo}>
               삭제
             </button>
           </li>
